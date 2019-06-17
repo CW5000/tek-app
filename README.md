724e3c4624fad31646b5e8d49df73a19a8f398a8
CW5000 <c_fries@sbcglobal.net>
2019-06-16 22:09:18 -0500

    Final bunch of changes.

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 I created this to support my ambitions to become a better-rounded SDET.
 
 running node index.js from the aforementioned file's directory starts the service.
-running Java -Jar karate-0.9.3.jar <test name>.feature (or whatever) runs the API test suite.
-running nightwatch ./UItests.js executes the UI nightwatch selenium tests.
+running java -Jar karate-0.9.3.jar <test name>.feature (or whatever) executes the API test suite.
+Nightwatch is a horrible piece of junk. I tried to go there with UI test for this, but ended up going with cypress.io. It is so much easier.
 if you have a copy of jmeter, you can run \tek-app\srv\tests\bystateabbrev.jmx for a quick server-side performance validation.
+In the interests of saving time, I created small sample front-end app outside the scope of a full-blown node project. To run it, you'll navigate to the directory hosting the file 'vue-sample.html' and run http-server (install this as a global node package, if you haven't already done so). This will run the aforementioned app from 'http://localhost:8081/vue-sample.html'. If I'd had more time to devote to learning how to build a proper vue project, I'd have gone about this more properly. 
+Once the sample app is up and running in the browser, you'll be able to kick the tires. I'm not a front-end developer, but this part was fun. You'll also be able to run the automated UI tests.
+I mentioned earlier I tried automating this using Nightwatch.js. It did not go well. Aside from critical and severe security vulnerabilities due to apparently no one being interested in updating dependencies, the tool itself was very difficult to work with. I eventually abandoned it and tried my luck at Cypress.io. This tool is really, really easy to work with. To run the tests, navigate to the project directory and run 'npm run cypress:open'. Tests will open in a separate, browser-based test runner and everything will be awesome.
--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,10 +1,48 @@
 describe('Search by valid state abbreviation', function() {
+    it('Verifies the existence of the Input field', function() {
+        cy.visit('http://localhost:8081/vue-sample.html')
+        cy.get('input')
+            .should('exist')
+    });
+
+    it('Verifies the existence of the Submit button', function() {
+        cy.visit('http://localhost:8081/vue-sample.html')
+        cy.get('button')
+            .should('exist')
+    });
+
+    /*This one is a work in progress...seems to have trouble locking on
+    to the field's text value after it being established... so
+    it's going to fail in execution. 
+    
+    All things considered, this isn't
+    something you'd use a test automation script to validate
+    because it's utility is limited beyond the sprint in which the test
+    is first introduced.*/
+    it('Verifies the character limit on the Input field', function() {
+        cy.visit('http://localhost:8081/vue-sample.html')
+        cy.get('[name="inputData"]')
+            .clear()
+            .type('01234567890123456789012345')
+        cy.get('[name="inputData"]')
+            .contains('01234567890123456789')
+    });
+
+    it('Verifies target state changeover', function() {
+        cy.visit('http://localhost:8081/vue-sample.html')
+        cy.get('input')
+            .clear()
+            .type('MO')
+        cy.get('[name="target"]')
+            .should('have.text', 'MO')
+    });
+
     it('Searches by state abbreviation', function() {
-      cy.visit('http://localhost:8081/vue-sample.html')
-      cy.get('input')
+        cy.visit('http://localhost:8081/vue-sample.html')
+        cy.get('input')
         .clear()
         .type('MO')
-      cy.get('button')
+        cy.get('button')
         .click()
         cy.get('[name="largest"]')
         .should('have.text', 'Kansas City')
@@ -15,13 +53,65 @@ describe('Search by valid state abbreviation', function() {
     it('Searches by state name', function() {
         cy.visit('http://localhost:8081/vue-sample.html')
         cy.get('input')
-          .clear()
-          .type('Missouri')
+            .clear()
+            .type('Missouri')
         cy.get('button')
-          .click()
-          cy.get('[name="largest"]')
-          .should('have.text', 'Kansas City')
-          cy.get('[name="capital"]')
-          .should('have.text', 'Jefferson City')
-      });
+            .click()
+            cy.get('[name="largest"]')
+            .should('have.text', 'Kansas City')
+            cy.get('[name="capital"]')
+            .should('have.text', 'Jefferson City')
+    });
+
+    it('Searches for an invalid string', function() {
+        cy.visit('http://localhost:8081/vue-sample.html')
+        cy.get('input')
+            .clear()
+            .type('AAA')
+        cy.get('button')
+            .click()
+            cy.get('[name="largest"]')
+            .should('have.text', 'No data found')
+            cy.get('[name="capital"]')
+            .should('have.text', 'No data found')
+    });
+
+    it('Searches for an invalid string', function() {
+        cy.visit('http://localhost:8081/vue-sample.html')
+        cy.get('input')
+            .clear()
+            .type('aa')
+        cy.get('button')
+            .click()
+            cy.get('[name="largest"]')
+            .should('have.text', 'No data found')
+            cy.get('[name="capital"]')
+            .should('have.text', 'No data found')
+    });
+
+    it('Searches for an invalid string', function() {
+        cy.visit('http://localhost:8081/vue-sample.html')
+        cy.get('input')
+            .clear()
+            .type('1$')
+        cy.get('button')
+            .click()
+            cy.get('[name="largest"]')
+            .should('have.text', 'No data found')
+            cy.get('[name="capital"]')
+            .should('have.text', 'No data found')
+    });
+
+    it('Searches for an invalid string', function() {
+        cy.visit('http://localhost:8081/vue-sample.html')
+        cy.get('input')
+            .clear()
+            .type('Whyyoming')
+        cy.get('button')
+            .click()
+            cy.get('[name="largest"]')
+            .should('have.text', 'No data found')
+            cy.get('[name="capital"]')
+            .should('have.text', 'No data found')
+    });
   })
